ba827790eee5389adf71268289b909d2bc3ba013
uxvu1196 <uxvu1196@gmail.com>
2021-10-22 16:47:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// PACKAGE.JSON: start-Bereich: man gibt heroku dem Befehl, dass er die Index.js immer automatisch ausführen sollte, statt das man im Terminal  manuell eingibt
+
+const express = require('express')
+const app = express()
+// const axios = require('axios'); um die API zu fetchen/abholen
+const mongoose = require('mongoose')
+//Du musst Mongoose erstmal implementieren, damit man das benutzen. es ist Cascading und sollte vor blog kommen
+const blog = require("./models/blog.js")
+require('dotenv').config()
+// const dbUri = `${process.env.MONGODB}` Befehl die Datenbank sollte benutzt werden
+const dbUri = `mongodb+srv://m001-student:${process.env.MONGODB_PW}@sandbox.lvxjs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+//==================MIDDLEWARES=====================
+//Das sind Methoden die wir Express hinzufügen, Methoden sind Funktionen die man von den Ordnern mit den ganzen Codes/function hier ins index.js importiert um sie dannzu benutzen 
+app.use(express.static('public'))
+// das ist dafür da dann man bein index.ejs nicht extra link public/assets klicken musst, sondern direkt assets/... schreiben kannst. Das ist ein Router
+app.set('view engine', 'ejs')
+// dass wir den view ordner benutzen und das wir ejs benutzen, ohne dem Funktioniert die Seite nicht, 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+//alles was du in express schreibst wird in json umgewandelt
+
+const port = process.env.PORT || 3050
+//3000 bis 18.000
+// ||oder  
+
+//=====================BROWSE THE SERVER WITH MONGOOSE===========================
+
+mongoose.connect(dbUri, () => {
+    console.log('Database is connected')
+    app.listen(port, () => {
+        console.log(`listening at http://localhost:${port}`)
+    })
+})
+
+//es wird gerendert / die Seite wird dann angezeigt / bzw. URL-Link
+//=====================ROUTING===================================================
+app.get('/', (req, res) => {
+    blog.find()
+        .then(results => {
+            console.log(results)
+            res.render('pages/index.ejs', { results })
+        })
+        .catch(err => console.log(err))
+})
+app.get('/detail/:id', (req, res) => {
+    blog.find()
+        .then(results => {
+            console.log(results)
+            res.render('pages/detail.ejs', { results })
+        })
+        .catch(err => console.log(err))
+})
+app.get('/add', (req, res) => {
+    blog.find()
+        .then(results => {
+            console.log(results)
+            res.render('pages/add.ejs', { results })
+        })
+        .catch(err => console.log(err))
+
+})
---
+++ models/blog.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+//const {Schema}=mongoose
+
+const blogSchema = new Schema({
+    // Diese id wird von Mogno automatisch selber beim Collection in Datebase Deployment vergeben, sie ist unique 
+    _id: {
+        type: Number,
+        required: true,
+    },
+    // Da kann man eine ID vergeben
+    id: {
+        type: Number,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    published_at: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    author_bild: {
+        type: String,
+        required: true,
+    }
+
+    //required:true heißt dass der Feld unbedingt eine Eingabe erfordert und nicht leer bleiben kann wie bei false 
+
+}, { timestamps: true })
+//Um zu kontrollieren wann es der Datenbank hinzugefügt worden ist oder geupdatet worden ist. 
+
+// Model based on the Schema
+//=> pluralize : GalleryDb => GalleryDbs
+const blog = mongoose.model('nordicRoseDB', blogSchema)
+
+//Bei nordicRoseDB kann man einfach ein Namen vergeben und da wird automatisch ein Ordner in dem Collections erstellt
+
+module.exports = blog
+//man exportieret die Datei
